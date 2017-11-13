225d8f7ab52a0ee08c9295b144bad6f59e84c559
iamsubho76 <iamsubho76@gmail.com>
2017-11-13 21:25:52 +0530

    updated

--- src/main/java/com/infotech/util/HibernateUtil.java
+++ src/main/java/com/infotech/util/HibernateUtil.java
@@ -1,24 +1,26 @@
 package com.infotech.util;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.io.File;
+import java.io.InputStream;
+import java.net.URL;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 	private static StandardServiceRegistry standardServiceRegistry;
 	private static SessionFactory sessionFactory;
-
+	private static Configuration cfg = null;
 	static {
 		if (sessionFactory == null) {
 			try {
+				URL resource = HibernateUtil.class.getResource("/hibernate_oracle.cfg.xml");
 				// Create StandardServiceRegistry
-				standardServiceRegistry = new StandardServiceRegistryBuilder().configure().build();
+				standardServiceRegistry = new StandardServiceRegistryBuilder().configure(new File(resource.toURI())).build();
 				// Create MetadataSources
 				MetadataSources metadataSources = new MetadataSources(standardServiceRegistry);
 				// Create Metadata
